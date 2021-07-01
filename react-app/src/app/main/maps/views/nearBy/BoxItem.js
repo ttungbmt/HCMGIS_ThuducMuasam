a9c9d59f2023b7934de2f4e31490f33a1ff879d2
Truong Thanh Tung <ttungbmt@gmail.com>
2021-07-01 09:51:26 +0700

    Update code

--- react-app/src/app/main/maps/views/nearBy/BoxItem.js
+++ react-app/src/app/main/maps/views/nearBy/BoxItem.js
@@ -1,8 +1,9 @@
-import React, {memo, useState} from "react";
+import React, {Fragment, memo, useState} from "react";
 import {ListItem, ListItemAvatar, Avatar, Box, Divider} from "@material-ui/core";
 import {toLatLng} from "@redux-leaflet";
 import {useDispatch} from "react-redux";
 import $emitter from 'app/utils/eventEmitter'
+import {includes} from 'lodash'
 
 function BoxItem({index, data, values}) {
     const dispatch = useDispatch()
@@ -25,6 +26,11 @@ function BoxItem({index, data, values}) {
         $emitter.emit('marker/selected', index)
     }
 
+    const openWeb = () => {
+        if(includes(data.web_shopping, 'http')) window.open(data.web_shopping)
+        else window.open(`http://`+data.web_shopping)
+    }
+
     return (
         <Box>
             <ListItem alignItems="flex-start" button className="py-10" onClick={onShowMarker}>
@@ -40,21 +46,37 @@ function BoxItem({index, data, values}) {
                     <Box className="text-xs text-gray-600" pb={1.2}>
                         {data.loaihinh}
                     </Box>
+                    {data.app_shopping && (
+                        <Box className="text-xs text-gray-800" style={{fontSize: 13}} pb={1.2}>
+                            <i className="fal fa-browser text-blue-500 pr-2"></i> {data.app_shopping}
+                        </Box>
+                    )}
+                    {data.hotline_shopping && (
+                        <Box className="text-xs text-gray-800" style={{fontSize: 13}} pb={1.2}>
+                            <i className="fal fa-phone text-blue-500 pr-2"></i> {data.hotline_shopping}
+                        </Box>
+                    )}
                     <Box className="text-xs text-gray-800" style={{fontSize: 13}} pb={1.2}>
-                        {data.diachi}
+                        <i className="fal fa-address-card text-blue-500 pr-2"></i> {data.diachi}
                     </Box>
                     <Box>
                         <button className="text-xs text-yellow-900" onClick={onShowMore}>{toggle ? 'Rút gọn' : 'Xem thêm'}</button>
                         <span className="px-8 text-gray-500" style={{fontSize: 10}}>|</span>
                         <button className="text-xs text-yellow-900" onClick={onDirection}>Chỉ đường</button>
+                        {data.web_shopping && (
+                            <Fragment>
+                                <span className="px-8 text-gray-500" style={{fontSize: 10}}>|</span>
+                                <button className="text-xs text-yellow-900" onClick={openWeb}>Mua sắm</button>
+                            </Fragment>
+                        )}
                     </Box>
                 </Box>
             </ListItem>
             {toggle && (
                 <Box mx={4} my={1} className="border-dashed border-2 border-light-gray-500 px-6 py-6 text-xs">
                     <div className="pb-4"><span className="font-medium">Thời gian hoạt động:</span> {data.tg_hoatdong}</div>
-                    <div className="pb-4"><span className="font-medium">Điện thoại:</span> {data.dienthoai}</div>
                     <div className="pb-4"><span className="font-medium">Hình thức giao trực tuyến:</span> {data.ht_giao_tt}</div>
+                    {/*<div className="pb-4"><span className="font-medium">Điện thoại người liên hệ:</span> {data.dienthoai}</div>*/}
                     {/*<div className="pb-4"><span className="font-medium">Thông tin liên hệ:</span> {data.tt_lienhe}</div>*/}
                     <div className="pb-4"><span className="font-medium">Phường xã:</span> {data.tenphuong}</div>
                     <div className="pb-4"><span className="font-medium">Phân tuyến hàng hóa:</span> {data.tuyen_cc}</div>
